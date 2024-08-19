11c29212e49da5d75a02467e33acb6b5b46d6c97
ChanukaIT22560544 <IT22560544@my.sliit.lk>
2024-08-19 14:34:16 +0530

    Skills

--- My Portfolio/my-portfolio/src/components/Skills.jsx
+++ My Portfolio/my-portfolio/src/components/Skills.jsx
@@ -8,6 +8,15 @@ const Skills = () => {
         <span className="m-2 px-4 py-2 bg-indigo-600 text-white rounded">HTML</span>
         <span className="m-2 px-4 py-2 bg-indigo-600 text-white rounded">CSS</span>
         <span className="m-2 px-4 py-2 bg-indigo-600 text-white rounded">JavaScript</span>
+        <span className="m-2 px-4 py-2 bg-indigo-600 text-white rounded">React.js</span>
+        <span className="m-2 px-4 py-2 bg-indigo-600 text-white rounded">Firebase</span>
+        <span className="m-2 px-4 py-2 bg-indigo-600 text-white rounded">Vite</span>
+        <span className="m-2 px-4 py-2 bg-indigo-600 text-white rounded">TilwindCss</span>
+        <span className="m-2 px-4 py-2 bg-indigo-600 text-white rounded">MogoDB</span>
+        <span className="m-2 px-4 py-2 bg-indigo-600 text-white rounded">Next.js</span>
+        <span className="m-2 px-4 py-2 bg-indigo-600 text-white rounded">PHP</span>
+        <span className="m-2 px-4 py-2 bg-indigo-600 text-white rounded">MySql</span>
+        <span className="m-2 px-4 py-2 bg-indigo-600 text-white rounded">Android Studio</span>
         {/* Add more skills similarly */}
       </div>
     </section>
