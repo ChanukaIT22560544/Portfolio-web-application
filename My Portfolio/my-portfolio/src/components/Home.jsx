b3833a9d50df0e48b0d6e8cfa9166170b50a9c6d
ChanukaIT22560544 <IT22560544@my.sliit.lk>
2024-08-20 01:01:32 +0530

    style

--- My Portfolio/my-portfolio/src/components/Home.jsx
+++ My Portfolio/my-portfolio/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import * as THREE from 'three';
+import './Home.css';
 
 
 const Home = () => {
@@ -12,9 +13,10 @@ const Home = () => {
     document.getElementById('three-container').appendChild(renderer.domElement);
 
     // Particle system
-    const particlesCount = 5000;
+    const particlesCount = 3000;
     const particlesGeometry = new THREE.BufferGeometry();
-    const particlesMaterial = new THREE.PointsMaterial({ color: 0xffffff, size: 0.05 });
+    const particlesMaterial = new THREE.PointsMaterial({ color: 0xffffff, size: 0.02 });
+    
 
     const particlesPosition = new Float32Array(particlesCount * 3);
     for (let i = 0; i < particlesCount * 3; i++) {
@@ -31,8 +33,8 @@ const Home = () => {
     const animate = () => {
       requestAnimationFrame(animate);
 
-      particles.rotation.x += 0.001;
-      particles.rotation.y += 0.001;
+      particles.rotation.x += 0.0008;
+      particles.rotation.y += 0.0008;
 
       renderer.render(scene, camera);
     };
@@ -54,17 +56,20 @@ const Home = () => {
       <div className="my-portfolio/public/my image.png"></div>
       <div id="three-container" className=""></div>
       <div className="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-0 absolute top-0 left-0">
-        <div className="text-center">
+        <div className="slide-in-left">
+      <img src="/my image.png" alt="my image" className="w-70 h-70 object-cover rounded-full" />
+       </div>
+
+        <div className="text-center slide-in-right">
           
-            
-          
-          <h1 className="text-white text-5xl font-bold ">Welcome to My Portfolio</h1><br/>
-          <h3 className="text-white text-5xl font-bold">I am Chanuka Lakshan</h3>
-          <p className="text-gray-300 mt-4">Discover my work and skills</p><br/>
-          <a href="#about" className="mt-6 px-4 py-2 bg-indigo-600 text-white font-bold rounded"> More</a>
-          <a href="/Chanuka Lakshan's CV.pdf" download className="m-6 px-4 py-2 bg-indigo-600 text-white font-bold rounded"> ⇩ CV  </a>
+          <h1 className="text-white text-6xl font-bold font-serif">Welcome to My Portfolio</h1><br/>
+          <h3 className="text-white text-5xl font-bold font-serif">I am Chanuka Lakshan</h3>
+          <p className="text-gray-300 mt-4 text-xl font-serif">Discover my work and skills</p><br/>
+          <a href="#about" className="mt-6 px-4 py-2 bg-indigo-600 text-white font-bold rounded font-serif"> More</a>
+          <a href="/Chanuka Lakshan's CV.pdf" download className="m-6 px-4 py-2 bg-indigo-600 text-white font-bold rounded font-serif"> ⇩ CV  </a>
          
         </div>
+
       </div>
     </section>
   );
