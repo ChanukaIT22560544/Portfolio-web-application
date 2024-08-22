07cd80d0241772a54f7c2c90fc072efbe107ec10
ChanukaIT22560544 <IT22560544@my.sliit.lk>
2024-08-22 18:13:07 +0530

    email

--- My Portfolio/my-portfolio/vite.config.js
+++ My Portfolio/my-portfolio/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['/node_modules/emailjs-com', 'react-toastify'],
+      external: ['emailjs-com', 'react-toastify'],
     },
   },
 })
