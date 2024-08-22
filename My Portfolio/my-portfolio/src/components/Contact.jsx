c2c48a46fa39c77c5d6b4e13d7a1ef4a6094ba7f
ChanukaIT22560544 <IT22560544@my.sliit.lk>
2024-08-22 17:50:03 +0530

    contact

--- My Portfolio/my-portfolio/src/components/Contact.jsx
+++ My Portfolio/my-portfolio/src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import emailjs from '/node_modules/emailjs-com';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
   const [formData, setFormData] = useState({
